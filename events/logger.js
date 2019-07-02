b4518f36e8bfdf78b5a3faaa4e8c84867bf28203
Keith Dahlby <keith@newbo.co>
2019-07-02 09:50:02 -0500

    Use correct socket.io  namespace

--- events/logger.js
+++ events/logger.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const io = require('socket.io-client');
-const socket = io.connect('http://localhost:3000/database');
+const socket = io.connect('http://localhost:3000');
 
 socket.on('file-save', data => {
   console.log('SAVE LOG', data.toString());
